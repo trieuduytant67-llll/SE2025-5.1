5791465348b087483b05728fa91adabe2756dfa3
trieuduytant67-llll <trieuduytan_t67@hus.edu.vn>
2025-12-10 00:10:14 +0700

    Add files via upload

---
+++ APP v0/script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector('#menu-icon');
+let navigation = document.querySelector('.navigation');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
